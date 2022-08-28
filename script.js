2fdf4b2856a58b8ac89d6eb6d6f3284f1612cc6a
Meen Philip <meenphilip@gmail.com>
2022-08-28 14:06:28 +0300

    initialize the DOM variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const  showMillionairesBtn= document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calcultaeWealthBtn = document.getElementById("calculate-wealth");
+
+let data = []
